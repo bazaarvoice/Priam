847efe37fa4896f4911d0a5e4d2fab65fc4a3c17
Delyan Tashev <Delyan.Tashev@bazaarvoice.com>
2015-02-17 13:31:20 +0000

    dtashev-EMO-5319-priam-allocates-existing-token

--- priam/src/test/java/com/netflix/priam/utils/BOPTokenManagerTest.java
+++ priam/src/test/java/com/netflix/priam/utils/BOPTokenManagerTest.java
@@ -264,7 +264,7 @@ public class BOPTokenManagerTest {
         BOPTokenManager tokenManager2 = new BOPTokenManager(50,
                 "555500112233445566778899aabbccddeeff00112233445566778899aabbccddeeff00112233445566778899aabbccddeeff",
                 "5555ffeeddccbbaa99887766554433221100ffeeddccbbaa99887766554433221100ffeeddccbbaa99887766554433221100");
-        assertEquals("555580000000000000000000000000000000000000000000000000000000000000000000000000000000000000001637af51",
+        assertEquals("555500112233445566778899aabbccddeeff00112233445566778899aabbccddeeff00112233445566778899aabbe3159e4f",
                 tokenManager2.createToken(1, 3, 2, "eu-west-1"));
     }
 
