782e462bd5cbb6f5689125c1ca1d8fd1bc460062
Mike Unitskyi <mykhailo.unitskyi@bazaarvoice.com>
2023-01-26 12:00:48 +0100

    PD-202549: bump plugins (#92)

--- priam-cass-extensions/src/main/java/com/netflix/priam/cassandra/extensions/DataFetcher.java
+++ priam-cass-extensions/src/main/java/com/netflix/priam/cassandra/extensions/DataFetcher.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2013 Netflix, Inc.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,7 +15,6 @@
  */
 package com.netflix.priam.cassandra.extensions;
 
-import com.google.common.base.Charsets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,6 +24,8 @@ import java.io.FilterInputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class DataFetcher {
     private static final Logger logger = LoggerFactory.getLogger(DataFetcher.class);
@@ -48,7 +49,7 @@ public class DataFetcher {
                 }
             }
             conn.disconnect();
-            String result = new String(bos.toByteArray(), Charsets.UTF_8);
+            String result = new String(bos.toByteArray(), UTF_8);
             logger.info("Calling URL API: {} returns: {}", url, result);
             return result;
 
